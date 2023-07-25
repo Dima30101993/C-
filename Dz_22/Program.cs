f1220778aba8671174f1514f70eae1dfdd8954db
Dima <fenchik30101993@gmail.com>
2023-07-25 09:57:01 +0300

    Решил 13 задачу из домашнего задания и чуть не сошел с ума

--- Dz_22/Program.cs
+++ Dz_22/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 && number > -100)
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
+else
+{
+    int finish = ThirdDigit(number);
+    Console.WriteLine($"Третья цифра числа {number} -> {finish}");
+
+}
+
+int ThirdDigit(int dg)
+{
+    int absNumber = Math.Abs(dg); 
+    string str = absNumber.ToString(); //решил немного поэксперементировать со строками, так как я питонист просьба камнями не кидать))))
+    int thirdDigit = (int)Char.GetNumericValue(str[2]);
+    return thirdDigit;
+}
