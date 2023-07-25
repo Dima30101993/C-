9618d711c7248eb2d896bd5e74dbf3b490908f6a
Dima <fenchik30101993@gmail.com>
2023-07-25 08:11:28 +0300

    Решение первой задачи второго урока

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая
+// выводит
+// случайное число из отрезка [10, 99], и показывает
+// наибольшую цифру числа
+
+// int number = new Random().Next(10, 100); // 99 + 1
+// Console.WriteLine($"Случайное число из отрезка 10 99 -> {number}");
+// int firstDigit = number / 10;
+// int secondDigit = number % 10;
+// if (firstDigit > secondDigit)
+// {
+//     Console.WriteLine($"Наибольшая цифра числа {firstDigit}");
+// }
+// else
+// {
+//     Console.WriteLine($"Наибольшая цифра числа {secondDigit}");
+// }
+
