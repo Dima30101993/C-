1d1328a53b026ace9863eddc38f72039de38c4e0
Dima <fenchik30101993@gmail.com>
2023-07-25 08:58:33 +0300

    Решил задачу 10 из домашнего задания

---
+++ Dz_21/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на выходе показывает вторую цифру этого числа
+
+Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000 && number > -1000)
+{
+    int secondDigit = ((number / 10) % 10);
+    Console.WriteLine(Math.Abs(secondDigit));
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
