43af8d2993e9f08386e45523a89b9a29db8b1770
Dima <fenchik30101993@gmail.com>
2023-07-25 10:48:08 +0300

    Решил 15 задачу из дамашнего задания

--- Dz_23/Program.cs
+++ Dz_23/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+
+bool dayWeek(int day)
+{
+    return day ==7 || day == 6;
+}
+Console.WriteLine("Введите номер дня недели:");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+bool result = dayWeek(dayNumber);
+Console.WriteLine(result? "День является выходным!" : "День не является выходным!!!");
+
+
+
+
+
+
+
+
+
+
+
+// if (dayNumber == 6 || dayNumber == 7)
+// {
+   
+//     Console.WriteLine($"{dayText} -> Выходной день!");
+// }
+// else
+// {
+   
+//     Console.WriteLine($"{dayText} -> Не выходной день!");
+// }
+
+
