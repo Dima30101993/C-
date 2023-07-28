73730d5814ba619726e20a57f929c699bdd68a36
Dima <fenchik30101993@gmail.com>
2023-07-28 10:53:29 +0300

    Выполнил 19 домашнюю задачу

--- Dz_31/Program.cs
+++ Dz_31/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// Выполнить с помощью числовых операций 
+// (целочисленное деление, остаток от деления).
+
+Console.WriteLine("Введите пятизначное число: ");
+Console.Write("Число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if(Number > 9999 && Number < 100000)
+{
+    bool result = Pal(Number);
+    Console.WriteLine(result 
+                        ? "Является палиндромом" 
+                        : "Не является палиндромом");
+}
+else
+{
+    Console.WriteLine("Введено неверное число");
+}
+
+bool Pal(int num)
+{
+   int number1 = num / 10000;
+   int number2 = num / 1000 % 10;
+   int number3 = num / 100 % 10;
+   int number4 = num / 10 % 10;
+   int number5 = num % 10;
+
+    return number1 == number5 && number2 == number4;
+}
