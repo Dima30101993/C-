86d5565a824024d4595351163f4a0e9b092be2f9
Dima <fenchik30101993@gmail.com>
2023-07-28 11:15:58 +0300

    .

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -43,23 +43,46 @@
 // координаты двух точек и находит расстояние между ними
 // в 2D пространстве.
 
-// 
+// Console.WriteLine("Введите координаты первой точки:");
+// Console.Write("X: ");
+// int x1Coord = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int y1Coord = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Z: ");
+// int z1Coord = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите координаты второй точки:");
+// Console.Write("X: ");
+// int x2Coord = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Y: ");
+// int y2Coord = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Z: ");
+// int z2Coord = Convert.ToInt32(Console.ReadLine());
 
-if(N < 1)
-{
-    Console.WriteLine("Некоректные данные");
-    return;
-}
-CubeTable(N);
+// double result = Distance(x1Coord, y1Coord, z1Coord, x2Coord, y2Coord, z2Coord);
+// double finish = Math.Round(result, 2, MidpointRounding.ToZero);
+// Console.WriteLine($"Расстояние между двух точек в 3D пространстве {finish}");
+// double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
+// {
+//     int dis = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
+//     return dis; 
+// }
 
-void CubeTable(int num)
-{
-    for(int i = 1; i <= num; i++)
-    {
-        int cube = i * i * i;
-        Console.WriteLine($"{i}    {cube}");
-    }
-}
+// Console.WriteLine("Введите число N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if(N < 1)
+// {
+//     Console.WriteLine("Некоректные данные");
+//     return;
+// }
+// CubeTable(N);
+
+// void CubeTable(int num)
+// {
+//     for(int i = 1; i <= num; i++)
+//     {
+//         int cube = i * i * i;
+//         Console.WriteLine($"{i}    {cube}");
+//     }
+// }
