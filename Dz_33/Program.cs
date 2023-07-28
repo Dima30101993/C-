c5e8371ffc4c5e8710c2100e5539769073d1e8b1
Dima <fenchik30101993@gmail.com>
2023-07-28 09:34:12 +0300

    Сделал домашнюю 23 задачу

--- Dz_33/Program.cs
+++ Dz_33/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if(N < 1)
+{
+    Console.WriteLine("Некоректные данные");
+    return;
+}
+CubeTable(N);
+
+void CubeTable(int num)
+{
+    for(int i = 1; i <= num; i++)
+    {
+        int cube = i * i * i;
+        Console.WriteLine($"{i}    {cube}");
+    }
+}
