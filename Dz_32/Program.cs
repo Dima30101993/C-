35db7a77ddbeacda7d9225ef1a73796f9c6b97a6
Dima <fenchik30101993@gmail.com>
2023-07-28 11:20:44 +0300

    Выполнил 21 домашнюю задачу

--- Dz_32/Program.cs
+++ Dz_32/Program.cs
@@ -17,6 +17,7 @@ int z2Coord = Convert.ToInt32(Console.ReadLine());
 double result = Distance(x1Coord, y1Coord, z1Coord, x2Coord, y2Coord, z2Coord);
 double finish = Math.Round(result, 2, MidpointRounding.ToZero);
 Console.WriteLine($"Расстояние между двух точек в 3D пространстве {finish}");
+
 double Distance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
     double dis = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2) + (z1 - z2)*(z1 - z2));
