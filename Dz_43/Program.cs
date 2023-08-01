8333dc77785c45a60499c5aeca6092305d746eff
Dima <fenchik30101993@gmail.com>
2023-08-01 10:15:32 +0300

    Сделал домашнюю задачу 29

---
+++ Dz_43/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// и выводит их на экран.
+
+int[] randomArray = new int[8];
+
+Array(randomArray);
+
+void Array(int[] ranA)
+{
+    for (int i = 0; i < 8; i++)
+    {
+        ranA[i] = new Random().Next(1,999);
+        Console.Write($"{ranA[i]} ");
+    }
+}
