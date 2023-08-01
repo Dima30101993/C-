70bfb743273118c5ac39062ae77b59c67d08d1b3
Dima <fenchik30101993@gmail.com>
2023-08-01 10:17:59 +0300

    Решал задачи по семинару

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает 
+// на вход число (А) и выдает сумму чисел
+// от 1 до А.
+
+// Console.WriteLine("Введите натуральное число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int finish = SumNumbers(number);
+// Console.WriteLine($"Сумма чисел от 1 до {number} -> {finish}");
+
+// int SumNumbers(int num)
+// {
+//     int sum = 0;
+//     for (int i = 0; i <= num; i++)
+//     {
+//         checked
+//         {
+//             sum += i;    
+//         }
+//     }
+//     return sum;
+// }
+
+// Напишите программу которая принимает на вход число
+// И выдает количество цифр в числе
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int finish = CountNumber(number);
+// Console.WriteLine($"Количество цифр в числе {number} -> {finish}");
+
+// int CountNumber(int num)
+// {
+//     int count = 0;
+//     while (num > 0 || num < 0)
+//     {
+//         num = num / 10;
+//         count ++;
+//     } 
+//     return count;
+// }
+
