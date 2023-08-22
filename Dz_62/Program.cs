9eecaf78b94e6d6d34c69df9473619462cc26931
Dima <fenchik30101993@gmail.com>
2023-08-22 18:47:54 +0300

    Решил домашнюю задачу 43

---
+++ Dz_62/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+int[] IntersectionPoint(int x1, int y1, int x2, int y2)
+{
+    int[] arr = new int[2];
+    int x = -(y1 - y2) / (x1 - x2);
+    int y = x1 * x + y1;
+    arr[0] = x;
+    arr[1] = y;
+    return arr;
+} 
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+       if(i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+       else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+
+Console.Write("Введите k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+
+int[] result = IntersectionPoint(k1, b1, k2, b2);
+Console.Write("Точка пересечения двух прямых A");
+PrintArray(result);
