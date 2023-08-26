26f51ffe4b7a7e2748cea2b7b238525bb1f98b0b
Dima <fenchik30101993@gmail.com>
2023-08-26 11:02:51 +0300

    Выполнил 52 домашнюю задачу

---
+++ Dz_73/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max) 
+{
+    int[,] matrix = new int[rows, columns];
+    
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 6}");
+        }
+        Console.WriteLine(); 
+    }
+    
+}
+
+void Average(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double average = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            average += matrix[i, j];
+        }
+    Console.Write($"{ Math.Round(average / matrix.GetLength(0), 1), 6} ");
+    }
+}
+int[,] array2d = CreateMatrixRndInt(3, 4, -100, 100);
+PrintMatrix(array2d);
+Console.WriteLine();
+
+Average(array2d);
+
